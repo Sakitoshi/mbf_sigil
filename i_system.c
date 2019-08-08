6282050bcf01a8bb769584f9896aff296c807935
Sakitoshi <sakitoshi@gmail.com>
2019-08-08 03:15:17 -0400

    fix endoom cursor position

--- i_system.c
+++ i_system.c
@@ -273,7 +273,7 @@ void I_EndDoom(void)
     {  // killough 8/19/98: simplify
       memcpy(0xb8000 + (byte *) __djgpp_conventional_base,
 	     W_CacheLumpNum(lump, PU_CACHE), 0xf00);
-      gotoxy(0,24);
+      gotoxy(1,25);
     }
 }
 
